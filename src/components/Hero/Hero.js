473c076ac9ec47e96c8ab3b1db65fcdc8c055d44
Matt Gainer <matt.gainer126@hotmail.com>
2021-09-03 11:24:39 -0400

    updates to create responsivness on mobile view

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,12 @@ function Hero() {
   return (
     <>
       <hero className="herowrapper">
-        <h2 className="hero">Hey,</h2>
-        <h2 className="hero">I'm Matt,</h2>
-        <h2 className="hero">web developer</h2>
-        <div className="hero__contact">Contact Me!</div>
+        <div>
+          <h2 className="hero">Hey,</h2>
+          <h2 className="hero">I'm Matt,</h2>
+          <h2 className="hero">web developer</h2>
+          <div className="hero__contact">Contact Me!</div>
+        </div>
       </hero>
     </>
   );
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,15 +4,17 @@ import PortfolioCards from "../PorfolioCards/PortfolioCards";
 function Portfolio() {
   return (
     <section className="port">
-      <h2 className="port__title">My Portfolio</h2>
-      <div className="port__summary-cta">
-        <p className="port__summary">
-          Here are a few samples of the projects that I've worked on
-        </p>
-        <div className="port__seemore">See More</div>
-      </div>
-      <div className="portfoliocards">
-        <PortfolioCards />
+      <div className="port__wrapper">
+        <h2 className="port__title">My Portfolio</h2>
+        <div className="port__summary-cta">
+          <p className="port__summary">
+            Here are a few samples of the projects that I've worked on
+          </p>
+          <div className="port__seemore">See More</div>
+        </div>
+        <div className="portfoliocards">
+          <PortfolioCards />
+        </div>
       </div>
     </section>
   );
