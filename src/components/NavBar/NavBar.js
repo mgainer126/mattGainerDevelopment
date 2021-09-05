a6cc9ef0064f66c7acf25f197921d79bb6ff543d
Matt Gainer <matt.gainer126@hotmail.com>
2021-09-05 17:08:55 -0400

    added linkage to github and linkedin to the nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,7 @@
 import "../NavBar/NavBar.scss";
 import logo from "../../assets/mattgainer.png";
+import githublogo from "../../assets/GitHub.png";
+import { ExternalLink } from "react-external-link";
 
 function NavBar() {
   return (
@@ -16,25 +18,18 @@ function NavBar() {
         </ul>
         <ul className="soc-media">
           <li>
-            <img
-              src="https://img.icons8.com/color/48/000000/linkedin.png"
-              alt="LinkedIn"
-              className="soc-media__img"
-            />
+            <ExternalLink href="https://www.linkedin.com/in/matthew-gainer-cbap-cspo/">
+              <img
+                src="https://img.icons8.com/color/48/000000/linkedin.png"
+                alt="LinkedIn"
+                className="soc-media__img"
+              />
+            </ExternalLink>
           </li>
           <li>
-            <img
-              src="https://img.icons8.com/color/48/000000/twitter--v2.png"
-              alt="twitter"
-              className="soc-media__img"
-            />
-          </li>
-          <li>
-            <img
-              src="https://img.icons8.com/fluency/48/000000/facebook.png"
-              alt="facebook"
-              className="soc-media__img"
-            />
+            <ExternalLink href="https://github.com/mgainer126">
+              <img src={githublogo} alt="github" className="soc-media__img" />
+            </ExternalLink>
           </li>
         </ul>
       </div>
