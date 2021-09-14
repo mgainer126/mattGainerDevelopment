cef990db7c9bc6085a41f84acf0ff95a58304b86
Matt Gainer <matt.gainer126@hotmail.com>
2021-09-14 17:40:43 -0400

    updates to about summary

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,10 +6,23 @@ function About() {
       <about className="about__wrapper">
         <h2 className="about__title">About Me</h2>
         <p className="about__summary">
-          My primary interest is front end. My passion for code begun working
-          closely with developers as a product owner. Watching concepts come to
-          life that help customers and users is truly exciting and very
-          gratifying.
+          Above all things, I love building things and making them work. There
+          is a moment after you have built something you have put your heart and
+          soul into, and you flip that switch and it works as expected. It's is
+          a big bang feeling that is hard to replicate, those who have worked
+          with me will attest to my joy of that moment. I've always gravitated
+          towards activities both personal and professional where I can create.
+        </p>
+        <p className="about__summary">
+          In my most recent role as a Product Owner Manager, I've had the
+          fortunate opportunity to work closely with developers as well as all
+          other areas of the business to bring ideas to life.
+        </p>
+        <p className="about__summary">
+          As a recent Web Development graduate I feel the sky is the limit in
+          terms of what I can do with code. With this educational knowledge, I
+          intend to deliver experiences to customers from inception through to
+          code as a developer. When you love what you do, it is not work.
         </p>
       </about>
     </section>
