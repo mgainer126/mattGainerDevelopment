e293eddbb0edbf092cb45c5c0bbff9063b76da00
Matt Gainer <matt.gainer126@hotmail.com>
2022-03-10 10:56:17 -0500

    updates to the skills page

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -2,28 +2,30 @@ import "../Skills/Skills.scss";
 
 function Skills() {
   return (
-    <div className="skills">
-      <h3 className="skills__title">Skills</h3>
-      <div className="skills__wrapper">
-        <section className="skills__grp">
-          <p className="skills__skill">HTML</p>
-          <p className="skills__skill">CSS</p>
-          <p className="skills__skill">SASS</p>
-          <p className="skills__skill">Javascript</p>
-          <p className="skills__skill">React.js</p>
-          <p className="skills__skill">Gatsby.js</p>
-          <p className="skills__skill">GraphQL</p>
-          <p className="skills__skill">DOM</p>
-          <p className="skills__skill">Web API</p>
-          <p className="skills__skill">Node.js</p>
-          <p className="skills__skill">MySql</p>
-          <p className="skills__skill">Knex</p>
-          <p className="skills__skill">Express.js</p>
-          <p className="skills__skill">GitHub</p>
-          <p className="skills__skill">Jira</p>
-          <p className="skills__skill">Confluence</p>
-          <p className="skills__skill">Ts2 (TSYS)</p>
-        </section>
+    <div className="skills__pagealign">
+      <div className="skills">
+        <h3 className="skills__title">Skills</h3>
+        <div className="skills__wrapper">
+          <section className="skills__grp">
+            <p className="skills__skill">HTML</p>
+            <p className="skills__skill">CSS</p>
+            <p className="skills__skill">SASS</p>
+            <p className="skills__skill">Javascript</p>
+            <p className="skills__skill">React.js</p>
+            <p className="skills__skill">Gatsby.js</p>
+            <p className="skills__skill">GraphQL</p>
+            <p className="skills__skill">DOM</p>
+            <p className="skills__skill">Web API</p>
+            <p className="skills__skill">Node.js</p>
+            <p className="skills__skill">MySql</p>
+            <p className="skills__skill">Knex</p>
+            <p className="skills__skill">Express.js</p>
+            <p className="skills__skill">GitHub</p>
+            <p className="skills__skill">Jira</p>
+            <p className="skills__skill">Confluence</p>
+            <p className="skills__skill">Ts2 (TSYS)</p>
+          </section>
+        </div>
       </div>
     </div>
   );
