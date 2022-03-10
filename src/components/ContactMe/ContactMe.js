1be7289478dea8aefc20f62d040fe34287bc1865
Matt Gainer <matt.gainer126@hotmail.com>
2022-03-10 10:47:42 -0500

    updates to contact me alignment

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -32,75 +32,77 @@ function ContactMe() {
   const handleShow = () => setShow(true);
 
   return (
-    <contact className="contact">
-      <div className="contact__wrapper">
-        <h3 className="contact__title">Contact Me</h3>
-        <p className="contact__summary">
-          Please reach out to me with any questions and I'll be sure to contact
-          you back as soon as possible
-        </p>
-        <form className="contact__form" onSubmit={sendEmail}>
-          <div className="contact__info">
-            <input
-              type="text"
-              className="form-control contact__name"
-              id="name"
-              name="name"
-              placeholder="Name"
-            />
-            <label htmlFor="name"></label>
-            <input
-              type="email"
-              className="form-control contact__email"
-              id="email"
-              name="email"
-              placeholder="Email"
-            />
-            <label htmlFor="name"></label>
-          </div>
-          <div className="contact__desc">
-            <input
-              type="text"
-              className="form-control contact__subject"
-              id="subject"
-              name="subject"
-              placeholder="Subject"
-            />
-            <label htmlFor="name"></label>
-            <input
-              type="text"
-              className="form-control contact__fulldesc"
-              id="description"
-              name="description"
-              placeholder="Description"
-            />
-            <label htmlFor="name"></label>
-          </div>
-          <button
-            type="submit"
-            value="Send"
-            className="contact__btn"
-            variant="primary"
-            onClick={handleShow}
-          >
-            Send Message
-          </button>
-          <Modal show={show} onHide={handleClose} animation={false}>
-            <Modal.Header closeButton>
-              <Modal.Title>Message Sent!!</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Your Message Has Been Sent!!</Modal.Body>
-            <Modal.Body>I'll Be In Contact You Shortly.</Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Close
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </form>
-      </div>
-      <img src={map} alt="map" className="contact__map"></img>
-    </contact>
+    <div className="contact__wrapper">
+      <contact className="contact">
+        <div>
+          <h3 className="contact__title">Contact Me</h3>
+          <p className="contact__summary">
+            Please reach out to me with any questions and I'll be sure to
+            contact you back as soon as possible
+          </p>
+          <form className="contact__form" onSubmit={sendEmail}>
+            <div className="contact__info">
+              <input
+                type="text"
+                className="form-control contact__name"
+                id="name"
+                name="name"
+                placeholder="Name"
+              />
+              <label htmlFor="name"></label>
+              <input
+                type="email"
+                className="form-control contact__email"
+                id="email"
+                name="email"
+                placeholder="Email"
+              />
+              <label htmlFor="name"></label>
+            </div>
+            <div className="contact__desc">
+              <input
+                type="text"
+                className="form-control contact__subject"
+                id="subject"
+                name="subject"
+                placeholder="Subject"
+              />
+              <label htmlFor="name"></label>
+              <input
+                type="text"
+                className="form-control contact__fulldesc"
+                id="description"
+                name="description"
+                placeholder="Description"
+              />
+              <label htmlFor="name"></label>
+            </div>
+            <button
+              type="submit"
+              value="Send"
+              className="contact__btn"
+              variant="primary"
+              onClick={handleShow}
+            >
+              Send Message
+            </button>
+            <Modal show={show} onHide={handleClose} animation={false}>
+              <Modal.Header closeButton>
+                <Modal.Title>Message Sent!!</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Your Message Has Been Sent!!</Modal.Body>
+              <Modal.Body>I'll Be In Contact You Shortly.</Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </form>
+        </div>
+        <img src={map} alt="map" className="contact__map"></img>
+      </contact>
+    </div>
   );
 }
 
