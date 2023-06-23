c870815ac23a7dd37b6d37be447b7be0476f51b1
Matthew Gainer <matthewgainer@matthewacstudio.lan>
2023-06-23 17:19:21 -0400

    update to file path

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,21 +1,29 @@
 import * as React from "react";
-import "primereact/resources/primereact.min.css"; 
-import {Hero, About, Education, Works, Skills, Work, Contact, Footer} from '../components';
+import "primereact/resources/primereact.min.css";
+import {
+  Hero,
+  About,
+  Education,
+  Works,
+  Skills,
+  Work,
+  Contact,
+  Footer,
+} from "../components";
 
-function Main () {
-
-    return (
-        <>
-            <Hero />
-            <About />
-            <Education />
-            <Work />
-            <Skills />
-            <Works />
-            <Contact />
-            <Footer />
-        </>
-    )
+function Main() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Education />
+      <Work />
+      <Skills />
+      <Works />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
 
 export default Main;
